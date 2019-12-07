bb6b6b43ecde2f9f6facc51c8b95f8b1f11a4570
marsr <richard.marsot@pega.com>
2019-12-07 07:51:27 -0500

    add cms support

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         defaultLayouts: { default: path.resolve("./src/components/layout.js") }
       }
     },
+    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
