4684acecf8fec4d8a9cf571c846a61f6fe1de63d
marsr <richard.marsot@pega.com>
2019-12-10 16:34:38 -0500

    fix 404

--- content/pages/404.js
+++ content/pages/404.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-import Layout from "../../src/components/layout";
-
 const NotFoundPage = () => (
-  <Layout>
+  <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <p>You just hit a route that doesn&#39;t exist</p>
+  </div>
 );
 
 export default NotFoundPage;
